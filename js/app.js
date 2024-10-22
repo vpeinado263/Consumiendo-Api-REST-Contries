f602e23f9c585b879c87ac04ea991642da66d82f
Victor Peinado <vpeinado263@gmail.com>
2024-10-21 22:38:10 -0300

    corregir funcion de busqueda

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 //Busqueda de País por nombre
-const searchInput = document.getElementById('buscar');
+const searchInput = document.getElementById('search');
 searchInput.addEventListener('input', () => {
-    const query = searchInput.ariaValueMax.toLocaleLowerCase();
+    const query = searchInput.value.toLowerCase();
     const articles = document.querySelectorAll('article');
 
     articles.forEach(article => {
-        const title = article.querySelector('h2').textContent.toLocaleLowerCase();
+        const title = article.querySelector('h2').textContent.toLowerCase();
         if (title.includes(query)) {
             article.style.display = 'block';
         } else {
